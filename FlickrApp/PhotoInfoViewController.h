bff2fd65040e0d159fe61970efa23c38e07861e2
Trent Ellingsen <trentellingsen@gmail.com>
2012-12-04 20:38:09 -0800

    added a detail class for the place

---
+++ FlickrApp/PhotoInfoViewController.h
@@ -0,0 +1,17 @@
+//
+//  PhotoInfoViewController.h
+//  FlickrApp
+//
+//  Created by Trent Ellingsen on 12/4/12.
+//  Copyright (c) 2012 Trent Ellingsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FlickrAPIKey.h"
+#import "FlickrFetcher.h"
+
+@interface PhotoInfoViewController : UITableViewController
+
+@property (nonatomic) NSArray *photoDetails;
+
+@end
--- FlickrApp/PhotoInfoViewController.m
+++ FlickrApp/PhotoInfoViewController.m
@@ -1,32 +1,20 @@
 //
-//  TopPlacesViewController.m
+//  PhotoInfoViewController.m
 //  FlickrApp
 //
 //  Created by Trent Ellingsen on 12/4/12.
 //  Copyright (c) 2012 Trent Ellingsen. All rights reserved.
 //
 
-#import "TopPlacesViewController.h"
 #import "PhotoInfoViewController.h"
 
-@interface TopPlacesViewController ()
-
-@property (nonatomic) NSArray *selectedPlacePhotoDetails;
+@interface PhotoInfoViewController ()
 
 @end
 
-@implementation TopPlacesViewController
+@implementation PhotoInfoViewController
 
-@synthesize places = _places;
-@synthesize selectedPlacePhotoDetails = _selectedPlacePhotoDetails;
-
-- (NSArray *)places
-{
-	if (!_places) {
-		_places = [FlickrFetcher topPlaces];
-	}
-	return _places;
-}
+@synthesize photoDetails = _photoDetails;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -40,6 +28,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,7 +46,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.places count];
+    return [self.photoDetails count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -60,27 +54,22 @@
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-	NSDictionary *place = (NSDictionary *)[self.places objectAtIndex:indexPath.row];
-	
-	NSLog(@"%@", [self.places objectAtIndex:indexPath.row]);
-	
-	cell.textLabel.text =[place objectForKey:@"woe_name"];
-	cell.detailTextLabel.text = [place objectForKey:@"_content"];
-	
+    // Configure the cell...
+    
     return cell;
 }
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-	[segue.destinationViewController setPhotoDetails:self.selectedPlacePhotoDetails];
-}
-
 
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    self.selectedPlacePhotoDetails = [FlickrFetcher photosInPlace:[self.places objectAtIndex:indexPath.row] maxResults:INT16_MAX];
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
 }
 
 @end
